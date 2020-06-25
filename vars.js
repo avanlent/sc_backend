63de89a5f2f75375ceac6a448f5c77eec26b067e
anthonyv987 <anthonyv987@protonmail.com>
2020-06-25 17:53:55 -0500

    fix untracked files

---
+++ vars.js
@@ -0,0 +1,15 @@
+module.exports = {
+    ROLES: {
+        asArray: ['USER', 'MODERATOR', 'ADMIN'],
+        asObj: {
+            USER: { value: 'USER' },
+            MODERATOR: { value: 'MODERATOR' },
+            ADMIN: { value: 'ADMIN' }
+        }
+    },
+    endorsedSets: [
+        "5ec0233fc699494c4881bceb",
+        "5ec2e0a61ddbed6074004961",
+        "5ec0231fc699494c4881bcea"
+    ]
+}
